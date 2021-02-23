3f92371b003a9a16d7ea78688ea9f38f6b703918
Anibal Santos <anibalsantosgo@gmail.com>
2021-02-23 20:06:15 +0100

    ng

--- src/app/shared/models/user/userRegister.ts
+++ src/app/shared/models/user/userRegister.ts
@@ -1,4 +1,8 @@
 export class UserRegister {
   name!: string;
+  surname!: string;
+  type!: [];
+  email!: string;
   password!: string;
+  rePassword!: string;
 }
--- src/app/views/user/register/register.component.ts
+++ src/app/views/user/register/register.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { UserRegister } from 'src/app/shared/models/user/userRegister';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,31 +16,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   public message: string = '';
+  public newUser: UserRegister = new UserRegister();
+  public name!: FormControl;
+  public surname!: FormControl;
+  public type!: FormControl;
+  public email!: FormControl;
+  public password!: FormControl;
+  public rePassword!: FormControl;
   public registerForm!: FormGroup;
-  constructor(private router: Router, private builder: FormBuilder) {
-    this.createForm();
-  }
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private builder: FormBuilder) {}
 
-  createForm() {
+  ngOnInit(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(55),
+    ]);
+    this.surname = new FormControl('', [
+      Validators.minLength(3),
+      Validators.maxLength(55),
+      Validators.pattern(/^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$/),
+    ]);
+    // TODO: types selectors ngFor select options, model ?
+    this.type = new FormControl('', Validators.required );
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]);
+    this.rePassword = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      // TODO: MustMatch ???
+    ]);
     this.registerForm = this.builder.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      type: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      rePassword: ['', Validators.required],
+      name: this.name,
+      surname: this.surname,
+      type: this.type,
+      email: this.email,
+      password: this.password,
+      rePassword: this.rePassword,
     });
   }
-
-  register(registerForm: FormGroup) {
-    if (registerForm.invalid) {
-      this.message = 'CHECK ERRORS';
-      console.log(this.message);
-    } else {
-      const email: string = registerForm.value.username;
-      const password: string = registerForm.value.password;
-    }
+  register() {
+    this.newUser.name = this.name.value;
+    this.newUser.surname = this.surname.value;
+    this.newUser.type = this.type.value;
+    this.newUser.email = this.email.value;
+    this.newUser.password = this.password.value;
+    this.newUser.rePassword = this.rePassword.value;
   }
 }
