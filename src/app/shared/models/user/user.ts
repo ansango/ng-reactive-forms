5009dde5247b847d395c44343ed8c7ec73ba7cda
Anibal Santos <anibalsantosgo@gmail.com>
2021-02-26 18:21:34 +0100

    user token

--- src/app/shared/models/user/user.ts
+++ src/app/shared/models/user/user.ts
@@ -1,8 +1,13 @@
 export class User {
   firstName!: string;
   lastName!: string;
-  type!: string;
+  type!: UserType;
   email!: string;
   password!: string;
   token!: string;
+}
+
+export enum UserType {
+  TOURIST,
+  COMPANY,
 }
--- src/app/shared/services/user-store.service.ts
+++ src/app/shared/services/user-store.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserStoreService {
+  private _token!: string;
+  constructor() {}
 
-  constructor() { }
+  set token(token: string) {
+    this._token = token;
+    localStorage.setItem('token', this._token);
+  }
+
+  get token() {
+    const localToken = localStorage.getItem('token');
+    const token = localToken === null ? this._token : localToken;
+    return token;
+  }
+
+  isLoggedIn() {
+    return this.token;
+  }
 }
